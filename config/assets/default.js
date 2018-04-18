6e6f51bfeef7c8a2f81cfa2d85eff3d98f49a78d
jaturongsutta <jaturong.sutta@gmail.com>
2018-04-18 10:02:07 +0700

    AdminLTE

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,20 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+
+        // AdminLTE
+        'public/lib/bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'public/lib/bower_components/font-awesome/css/font-awesome.min.css',
+        'public/lib/bower_components/Ionicons/css/ionicons.min.css',
+        'public/lib/dist/css/AdminLTE.min.css',
+        'public/lib/dist/css/skins/_all-skins.min.css',
+        'public/lib/bower_components/morris.js/morris.css',
+        'public/lib/bower_components/jvectormap/jquery-jvectormap.css',
+        'public/lib/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
+        'public/lib/bower_components/bootstrap-daterangepicker/daterangepicker.css',
+        'public/lib/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
+        
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,7 +29,25 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+
+        //// AdminLTE
+        'public/lib/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'public/lib/bower_components/raphael/raphael.min.js',
+        'public/lib/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js',
+        'public/lib/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+        'public/lib/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+        'public/lib/bower_components/jquery-knob/dist/jquery.knob.min.js',
+        'public/lib/bower_components/moment/min/moment.min.js',
+        'public/lib/bower_components/bootstrap-daterangepicker/daterangepicker.js',
+        'public/lib/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+        'public/lib/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+        'public/lib/bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
+        'public/lib/bower_components/fastclick/lib/fastclick.js',
+        'public/lib/dist/js/adminlte.min.js',
+        'public/lib/dist/js/pages/dashboard.js',
+        'public/lib/dist/js/demo.js',
+
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
